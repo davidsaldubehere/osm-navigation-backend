d21714c1aeed092436033682c3bd2f56d907b1c5
David Saldubehere <davesaldub@gmail.com>
2024-05-29 20:22:50 -0400

    added high speed limit checking

--- classify_edges.py
+++ classify_edges.py
@@ -7,5 +7,12 @@ import matplotlib.pyplot as plt
 #from kmeans import run_plot
 from dbscan import run_plot, run_no_plot
 
+#returns edges with a speed limit higher than the threshold
 def high_speed_limit(osm, threshold=55):
-    
+    # Get the edges of the driving network
+    nodes, edges = osm.get_network(nodes=True, network_type="driving")
+    # Get the edges with a speed limit higher than the threshold
+    #Max speed is formatted weird like 25 mph or None
+
+    high_speed_edges = edges[edges['maxspeed'].apply(lambda x: int(x.split(' ')[0]) if x is not None else 0) >= threshold]
+    return high_speed_edges
--- testing.py
+++ testing.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from shapely.geometry import LineString
 from create_polygons import create_tree_boundary, create_tall_boundary, create_building_boundary
+from classify_edges import high_speed_limit
 osm = OSM("state_college_mountains.osm.pbf")
 
+high = high_speed_limit(osm)
+
+high.plot(figsize=(6,6), color="gray")
 
 
 buildings = create_building_boundary(osm)
